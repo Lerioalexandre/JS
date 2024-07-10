4be3738ac4a6bd2e9330249240e8b050cec216b1
Lério manuel <lerioa.m@g.mailcom>
2024-07-10 14:49:28 +0100

    Atualização  de script

--- EX01/js.js
+++ EX01/js.js
@@ -3,7 +3,7 @@ window.alert("O Site questa a ver foi feito por Lério A.M")
   // A linha acima indica um simles aviso
 
 window.confirm("Está gostando do nosso site?")
-// O codigo acima indica jum alerta de confirmação
+// O codigo acima indica um alerta de confirmação
 
 window.prompt("De onde vc acesa o noso site?")
 //Faz uma pergunta e agurada a resposta   
--- EX03/js.js
+++ EX03/js.js
@@ -1,9 +1,8 @@
-var n1= Number.parseFloat(prompt("Digite um número"))
-var n2= Number.parseFloat(prompt("Digite um número"))
+var n1= Number(prompt("Digite um número"))
+var n2= Number(prompt("Digite um número"))
 var soma= n1+n2
-alert("A soma é igual a,"+ soma) 
+//alert("A soma é igual a,"+ soma) 
 
-/*O alerta abaixo é uma alternativa ao de cima mas*/
-/*
+/*O alerta abaixo é uma alternativa ao de cima */
+
 window.alert(`A soma de ${n1} com ${n2} resulta em  ${soma} `)
-*/
